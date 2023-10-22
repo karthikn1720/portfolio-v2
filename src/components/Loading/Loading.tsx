ace555d4b1558f48a44eb79511a5d20f0c97b327
karthik1720 <96880552+karthik1720@users.noreply.github.com>
2023-10-22 13:03:12 +0530

    test workflow

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./loading.scss";
 const Loading = () => {
   const [state, setState] = React.useState<string | undefined>(undefined);
-
+  //
   React.useEffect(() => {
     setTimeout(() => {
       setState("trigger-animation");
