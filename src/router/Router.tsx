b66e383078db486599669b04eef22bf0e31e180d
karthik1720 <96880552+karthik1720@users.noreply.github.com>
2023-10-22 16:14:22 +0530

    remove loader

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,8 @@
 import MainLayout from "../layouts/Main/MainLayout";
-import Loading from "components/Loading/Loading";
 
 const Router = () => {
   return (
     <div className="routes-root">
-      <Loading />
       <MainLayout />
     </div>
   );
