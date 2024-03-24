77cc9f8b7ae2de13545291006235e1b22d0227d4
karthikn1720 <karthikn1720@gmail.com>
2024-03-24 18:08:13 +0530

    gha fix

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,18 +1,17 @@
 import MainLayout from "../layouts/Main/MainLayout";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <MainLayout />,
+//   },
+// ]);
 
 const Router = () => {
   return (
     <div className="routes-root">
-      {/* <MainLayout /> */}
-      <RouterProvider router={router}></RouterProvider>
+      <MainLayout />
+      {/* <RouterProvider router={router}></RouterProvider> */}
     </div>
   );
 };
